434bc235f3b9977a8faeae814fecb6db834a8842
John Grattan <contact@johngrattan.com>
2020-09-09 02:43:02 -0400

    Running audits on preview v.1.5

--- src/components/AboutNav/AboutNav.js
+++ src/components/AboutNav/AboutNav.js
@@ -12,7 +12,7 @@ const AboutNav = () => {
       noGutters
       className="about-dropdown about-dropdown-menu px-0 py-5 mt-0 mb-5 rounded drop-shadow text-center"
     >
-      <Col xs={6} md={6} lg={3} className="mb-5 mb-lg-0 px-1 px-lg-0">
+      <Col xs={6} md={6} lg={3} className="mb-5 mb-lg-0 px-1 px-lg-3">
         <Link to="/who-we-are">
           <div className="text-center p-1 p-md-3 bg-dark-gray">
             <ImgMdhTruckMenu className="border rounded" />
@@ -26,7 +26,7 @@ const AboutNav = () => {
           We Are
         </Link>
       </Col>
-      <Col xs={6} md={6} lg={3} className="mb-5 mb-lg-0 px-1">
+      <Col xs={6} md={6} lg={3} className="mb-5 mb-lg-0 px-1 px-lg-3">
         <Link to="/what-we-do">
           <div className="text-center p-1 px-0 p-md-3 bg-dark-gray">
             <ImgLicensedMenu className="border rounded" />
@@ -36,11 +36,10 @@ const AboutNav = () => {
           className="nav-link px-3 py-2 text-uppercase text-center bg-primary text-white font-weight-bold"
           to="/what-we-do"
         >
-          Licensing <br />
-          And Insurance
+          Licensing <br />& Insurance
         </Link>
       </Col>
-      <Col xs={6} md={6} lg={3} className="mb-5 mb-lg-0 px-1">
+      <Col xs={6} md={6} lg={3} className="mb-5 mb-lg-0 px-1 px-lg-3">
         <Link to="/ratings-and-memberships">
           <div className="text-center p-1 px-0 p-md-3 bg-dark-gray">
             <ImgTopRatedMenu className="border rounded" />
@@ -54,7 +53,7 @@ const AboutNav = () => {
           Memberships
         </Link>
       </Col>
-      <Col xs={6} md={6} lg={3} className="mb-5 mb-lg-0 px-1">
+      <Col xs={6} md={6} lg={3} className="mb-5 mb-lg-0 px-1 px-lg-3">
         <Link to="/service-area">
           <div className="text-center p-1 px-0 p-md-3 bg-dark-gray">
             <ImgServiceAreaMenu className="border rounded" />

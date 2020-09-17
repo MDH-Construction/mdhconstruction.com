8090bb562eb8b22fc85580eb639bd68afad699a1
John Grattan <contact@johngrattan.com>
2020-09-17 16:09:25 -0400

    Updated Thank You page w Notice

--- src/components/Page__ThankYou/ThankYou__Body.js
+++ src/components/Page__ThankYou/ThankYou__Body.js
@@ -6,16 +6,38 @@ import Button from '../Button/Button';
 const ThankYou__Body = ({ className }) => {
   return (
     <section id="thank-you-page">
-      <Row>
-        <Col className="py-3 py-lg-5" xs={12} md={12}>
-          <Container className="text-center">
-            <Link to="/">
-              <i className="fas fa-mobile-4x fa-home text-secondary pointer mb-4"></i>
-              <p className="display-4">Return To Our Homepage</p>
-            </Link>
-          </Container>
-        </Col>
-      </Row>
+      <Container>
+        <Row>
+          <Col className="py-3 mx-auto" xs={12} md={12} lg={10}>
+            <Container className="text-center">
+              <Container className="mb-5 p-5 bg-white border rounded drop-shadow">
+                <p className="lead text-danger font-weight-bold mb-5">
+                  IMPORTANT NOTICE:
+                </p>
+                <p className="lead text-dark font-weight-bold">
+                  We greatly appreciate your interest and look forward to
+                  working with you.
+                </p>
+                <p className="lead mb-5">
+                  At the moment, we are approximately 3 weeks out before
+                  approaching any new in-home estimates or projects.
+                </p>
+                <p className="lead font-italic text-right mb-0">
+                  Thanks,
+                  <br />
+                  MDH Construction
+                </p>
+              </Container>
+              <Container className="pt-5">
+                <Link to="/" className="mt-5 pt-5">
+                  <i className="fas fa-mobile-4x fa-home text-secondary pointer mb-4"></i>
+                  <p className="display-4">Return To Our Homepage</p>
+                </Link>
+              </Container>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 };

c2cc0c26569b9247a257d38e555db153d178e1fc
John Grattan <contact@johngrattan.com>
2021-02-23 16:40:43 -0500

    Added service page links

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -7,6 +7,7 @@ import Layout from '../components/layout';
 import SEO from '../components/common/SEO';
 import Header from '../components/layouts/headers/Header';
 import Banner from '../components/layouts/bodies/Banner';
+import CardImage from '../components/common/cards/CardImage';
 import ImgServicesFullConstruction from '../components/images/pages/services/img-services-full-construction';
 import ImgServicesNewAdditions from '../components/images/pages/services/img-services-new-additions';
 import ImgServicesRenovations from '../components/images/pages/services/img-services-renovations';
@@ -71,7 +72,7 @@ const ServicesPage = ({ pageContext, location }) => {
         Tag="div"
         className=""
         fluid={imageDataBodyBanner}
-        alt="A hammer, nailes, ruler, and blueprints laying on a wooden table."
+        alt="A hammer, nails, ruler, and blueprints laying on a wooden table."
         textLeftOne="Got any questions?"
         textLeftTwo="Give us a call!"
         textRight={
@@ -101,179 +102,209 @@ const ServicesPage = ({ pageContext, location }) => {
       <section className="section-container" id="services-page">
         <Container>
           <Row className="py-5 px-3 px-lg-0">
-            <Col xs={12} lg={4} className="mx-0 px-3 mb-5 mb-lg-0">
-              <Container className="bg-white drop-shadow rounded mx-0 px-0">
-                <h2 className="bg-secondary text-white py-3 mb-0">
-                  Full <br />
-                  Construction
-                </h2>
-                <Container className="p-0">
-                  <ImgServicesFullConstruction />
-                  <Container className="container-text px-3">
-                    <ul className="check pl-4">
-                      <li>Residential homes</li>
-                      <li>Commercial buildings</li>
-                      <li>Detached garages</li>
-                    </ul>
-                  </Container>
-                </Container>
-              </Container>
+            <Col xs={12} lg={4} className="mx-0 px-3">
+              <CardImage
+                title={
+                  <>
+                    Full <br />
+                    Construction
+                  </>
+                }
+                list={
+                  <>
+                    <li>Residential homes</li>
+                    <li>Commercial buildings</li>
+                    <li>Detached garages</li>
+                  </>
+                }
+                anchor="See Full Construction Services"
+                link="#"
+              >
+                <ImgServicesFullConstruction />
+              </CardImage>
             </Col>
 
             <Col xs={12} lg={4} className="mx-0 px-3 mb-5 mb-lg-0">
-              <Container className="bg-white drop-shadow rounded mx-0 px-0">
-                <h2 className="bg-secondary text-white py-3 mb-0">
-                  New <br />
-                  Additions
-                </h2>
-                <Container className="p-0">
-                  <ImgServicesNewAdditions />
-                  <Container className="container-text px-3">
-                    <ul className="check pl-4">
-                      <li>Rooms, living spaces, attics</li>
-                      <li>Additional floors</li>
-                      <li>Attached garages</li>
-                    </ul>
-                  </Container>
-                </Container>
-              </Container>
+              <CardImage
+                title={
+                  <>
+                    New <br />
+                    Additions
+                  </>
+                }
+                list={
+                  <>
+                    <li>Rooms, living spaces, attics</li>
+                    <li>Additional floors</li>
+                    <li>Attached garages</li>
+                  </>
+                }
+                anchor="See New Addition Services"
+                link="#"
+              >
+                <ImgServicesNewAdditions />
+              </CardImage>
             </Col>
 
             <Col xs={12} lg={4} className="mx-0 px-3">
-              <Container className="bg-white drop-shadow rounded mx-0 px-0">
-                <h2 className="bg-secondary text-white py-3 mb-0">
-                  Renovations <br />& Remodeling
-                </h2>
-                <Container className="p-0">
-                  <ImgServicesRenovations />
-                  <Container className="container-text px-3">
-                    <ul className="check pl-4">
-                      <li>Large interior renovations</li>
-                      <li>Kitchens, bathrooms, basements</li>
-                      <li>Custom trim & molding</li>
-                    </ul>
-                  </Container>
-                </Container>
-              </Container>
+              <CardImage
+                title={
+                  <>
+                    Renovations <br />& Remodeling
+                  </>
+                }
+                list={
+                  <>
+                    <li>Large interior renovations</li>
+                    <li>Kitchens, bathrooms, basements</li>
+                    <li>Custom trim & molding</li>
+                  </>
+                }
+                anchor="See Remodeling Services"
+                link="#"
+              >
+                <ImgServicesRenovations />
+              </CardImage>
             </Col>
           </Row>
 
           <Row className="pt-lg-5 px-3 px-lg-0 pb-lg-5 pt-0">
             <Col xs={12} lg={4} className="mx-0 px-3 mb-5 mb-lg-0">
-              <Container className="bg-white drop-shadow rounded mx-0 px-0">
-                <h2 className="bg-secondary text-white py-3 mb-0">
-                  Gutters &<br /> Downspouts
-                </h2>
-                <Container className="p-0">
-                  <ImgServicesGutters />
-                  <Container className="container-text px-3">
-                    <ul className="check pl-4">
-                      <li>Gutter installation, cleaning, repair</li>
-                      <li>Seamless gutter installation</li>
-                      <li>Downspout installation</li>
-                    </ul>
-                  </Container>
-                </Container>
-              </Container>
+              <CardImage
+                title={
+                  <>
+                    Gutters &<br /> Downspouts
+                  </>
+                }
+                list={
+                  <>
+                    <li>Gutter installation</li>
+                    <li>Gutter cleaning</li>
+                    <li>Gutter repair, replacement</li>
+                    <li>Seamless gutter installation</li>
+                    <li>Downspout installation</li>
+                  </>
+                }
+                anchor="See Gutter Services"
+                link="/gutters/"
+              >
+                <ImgServicesGutters />
+              </CardImage>
             </Col>
 
             <Col xs={12} lg={4} className="mx-0 px-3 mb-5 mb-lg-0">
-              <Container className="bg-white drop-shadow rounded mx-0 px-0">
-                <h2 className="bg-secondary text-white py-3 mb-0">
-                  Insulation &<br /> Weatherization
-                </h2>
-                <Container className="p-0">
-                  <ImgServicesInsulation />
-                  <Container className="container-text px-3">
-                    <ul className="check pl-4">
-                      <li>Mass Save insulation program</li>
-                      <li>Batt & rolled, reflective, blown-in</li>
-                      <li>Air sealing, weather stripping</li>
-                      <li>Thermal imaging, blower door diagnostics</li>
-                    </ul>
-                  </Container>
-                </Container>
-              </Container>
+              <CardImage
+                title={
+                  <>
+                    Insulation &<br /> Weatherization
+                  </>
+                }
+                list={
+                  <>
+                    <li>Mass Save insulation program</li>
+                    <li>Batt & rolled, reflective, blown-in</li>
+                    <li>Air sealing, weather stripping</li>
+                    <li>Thermal imaging, blower door diagnostics</li>
+                  </>
+                }
+                anchor="See Insulation Services"
+                link="#"
+              >
+                <ImgServicesInsulation />
+              </CardImage>
             </Col>
 
             <Col xs={12} lg={4} className="mx-0 px-3 mb-5 mb-lg-0">
-              <Container className="bg-white drop-shadow rounded mx-0 px-0">
-                <h2 className="bg-secondary text-white py-3 mb-0">
-                  Windows <br />& Doors
-                </h2>
-                <Container className="p-0">
-                  <ImgServicesWindows />
-                  <Container className="container-text px-3">
-                    <ul className="check pl-4">
-                      <li>New window installation</li>
-                      <li>Replacement window installation</li>
-                      <li>Interior door installation</li>
-                      <li>Exterior door installation</li>
-                    </ul>
-                  </Container>
-                </Container>
-              </Container>
+              <CardImage
+                title={
+                  <>
+                    Windows <br />& Doors
+                  </>
+                }
+                list={
+                  <>
+                    <li>New window installation</li>
+                    <li>Replacement window installation</li>
+                    <li>Interior door installation</li>
+                    <li>Exterior door installation</li>
+                    <li>Window & door repair</li>
+                  </>
+                }
+                anchor="See Window & Door Services"
+                link="#"
+              >
+                <ImgServicesWindows />
+              </CardImage>
             </Col>
           </Row>
 
           <Row className="pt-lg-5 px-3 px-lg-0 pb-lg-5 pt-0">
             <Col xs={12} lg={4} className="mx-0 px-3 mb-5 mb-lg-0">
-              <Container className="bg-white drop-shadow rounded mx-0 px-0">
-                <h2 className="bg-secondary text-white py-3 mb-0">
-                  Roofing <br />& Siding
-                </h2>
-                <Container className="p-0">
-                  <ImgServicesRoofing />
-                  <Container className="container-text px-3">
-                    <ul className="check pl-4">
-                      <li>Roof installation, replacement, repair</li>
-                      <li>Asphalt & wood shingles</li>
-                      <li>Siding installation, replacement, repair</li>
-                      <li>Vinyl, wood, fiber-cement siding</li>
-                    </ul>
-                  </Container>
-                </Container>
-              </Container>
+              <CardImage
+                title={
+                  <>
+                    Roofing <br />& Siding
+                  </>
+                }
+                list={
+                  <>
+                    <li>Roof installation, replacement, repair</li>
+                    <li>Asphalt & wood shingles</li>
+                    <li>Siding installation, replacement, repair</li>
+                    <li>Vinyl, wood, fiber-cement siding</li>
+                  </>
+                }
+                anchor="See Roofing & Siding Services"
+                link="#"
+              >
+                <ImgServicesRoofing />
+              </CardImage>
             </Col>
 
             <Col xs={12} lg={4} className="mx-0 px-3 mb-5 mb-lg-0">
-              <Container className="bg-white drop-shadow rounded mx-0 px-0">
-                <h2 className="bg-secondary text-white py-3 mb-0">
-                  Outdoor <br />
-                  Living
-                </h2>
-                <Container className="p-0">
-                  <ImgServicesDecks />
-                  <Container className="container-text px-3">
-                    <ul className="check pl-4">
-                      <li>Deck construction</li>
-                      <li>Porch construction</li>
-                      <li>Fencing installation</li>
-                      <li>Retainer wall installation</li>
-                    </ul>
-                  </Container>
-                </Container>
-              </Container>
+              <CardImage
+                title={
+                  <>
+                    Outdoor <br />
+                    Living
+                  </>
+                }
+                list={
+                  <>
+                    <li>Deck construction</li>
+                    <li>Porch construction</li>
+                    <li>Fencing installation</li>
+                    <li>Retainer wall installation</li>
+                  </>
+                }
+                anchor="See Outdoor Living Services"
+                link="#"
+              >
+                <ImgServicesDecks />
+              </CardImage>
             </Col>
 
             <Col xs={12} lg={4} className="mx-0 px-3 mb-5 mb-lg-0">
-              <Container className="bg-white drop-shadow rounded mx-0 px-0">
-                <h2 className="bg-secondary text-white py-3 mb-0">
-                  Other <br />
-                  Services
-                </h2>
-                <Container className="p-0">
-                  <ImgServicesPainting />
-                  <Container className="container-text px-3">
-                    <ul className="check pl-4">
-                      <li>Construction management</li>
-                      <li>Interior and exterior painting</li>
-                      <li>Debris removal, clean out, haul away</li>
-                      <li>Handyman services</li>
-                    </ul>
-                  </Container>
-                </Container>
-              </Container>
+              <CardImage
+                title={
+                  <>
+                    Other <br />
+                    Services
+                  </>
+                }
+                list={
+                  <>
+                    <li>Construction management</li>
+                    <li>Interior and exterior painting</li>
+                    <li>Debris removal, clean out, haul away</li>
+                    <li>Handyman services</li>
+                  </>
+                }
+                anchor="See Our Other Services"
+                link="#"
+              >
+                <ImgServicesPainting />
+              </CardImage>
             </Col>
           </Row>
         </Container>

aceb5439512a8498fd123651436d39e7ce4cf0a3
John Grattan <contact@johngrattan.com>
2021-07-07 21:59:52 -0400

    Added Buildzoom award

--- src/components/views/home/Trust.jsx
+++ src/components/views/home/Trust.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 import { Container, Row, Col } from 'react-bootstrap';
 import BackgroundImage from 'gatsby-background-image';
 import ImgHomeAdvisor from '../../images/logos/img-home-advisor';
@@ -20,10 +21,52 @@ const Trust = ({ className }) => {
           }
         }
       }
+      imgBuildzoom: file(
+        relativePath: {
+          eq: "images/top-5-general-contractor-buildzoom-2021-mdh-construction-plymouth-ma.png"
+        }
+      ) {
+        id
+        childImageSharp {
+          fixed(quality: 90, height: 126) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      mobileImgBuildzoom: file(
+        relativePath: {
+          eq: "images/top-5-general-contractor-buildzoom-2021-mdh-construction-plymouth-ma.png"
+        }
+      ) {
+        childImageSharp {
+          fixed(quality: 90, height: 75) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+      desktopImgBuildzoom: file(
+        relativePath: {
+          eq: "images/top-5-general-contractor-buildzoom-2021-mdh-construction-plymouth-ma.png"
+        }
+      ) {
+        childImageSharp {
+          fixed(quality: 90, height: 126) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
     }
   `);
 
   const imageData = data.masthead.childImageSharp.fluid;
+  const imgBuildzoom = data.imgBuildzoom.childImageSharp.fixed;
+  const sourcesBuildzoom = [
+    data.mobileImgBuildzoom.childImageSharp.fixed,
+    {
+      ...data.desktopImgBuildzoom.childImageSharp.fixed,
+      media: `(min-width: 768px)`,
+    },
+  ];
 
   return (
     <BackgroundImage
@@ -39,17 +82,37 @@ const Trust = ({ className }) => {
           <span className="font-weight-bold">Top Industry Organizations</span>:
         </span>
         <hr className="divider mt-2 pb-3 drop-shadow-darker" />
-        <Row className="justify-content-center">
-          <Col className="mb-3 mb-lg-0 mx-auto" xs={'6'} lg={4}>
+        <div className="flex text-center mx-auto">
+          <Img
+            fixed={sourcesBuildzoom}
+            alt="MDH Construction has been awarded Top 5 General Contractor in Plymouth by Buildzoom"
+            className="mx-4 align-items-center align-content-center"
+          />
+        </div>
+        <div className="pt-lg-3">
+          <div className="flex text-center mx-auto">
+            <ImgBBB className="mx-4" />
+            <ImgAngiesList className="mx-4" />
+            <ImgHomeAdvisor className="mx-4" />
+          </div>
+        </div>
+        {/* <Row className="flex justify-content-center">
+          <Col className="mb-3 mb-lg-0 mx-auto" xs={'6'} lg={2}>
             <ImgBBB />
           </Col>
-          <Col className="mb-3 mb-lg-0 mx-auto" xs={'6'} lg={4}>
+          <Col className="mb-3 mb-lg-0 mx-auto" xs={'6'} lg={6}>
+            <Img
+              fixed={imgBuildzoom}
+              alt="MDH Construction has been awarded Top 5 General Contractor in Plymouth by Buildzoom"
+            />
+          </Col>
+          <Col className="mb-3 mb-lg-0 mx-auto" xs={'6'} lg={2}>
             <ImgAngiesList />
           </Col>
-          <Col className="mb-3 mb-lg-0 mx-auto" xs={'6'} lg={4}>
+          <Col className="mb-3 mb-lg-0 mx-auto" xs={'6'} lg={2}>
             <ImgHomeAdvisor />
           </Col>
-        </Row>
+        </Row> */}
       </Container>
     </BackgroundImage>
   );
